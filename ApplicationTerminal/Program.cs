6b8572c05a52ff9b48047f9b008d5867a9fecfb3
Lucas Freitas <lucas@xsys.com.br>
2022-06-27 10:10:03 -0300

    Add project files.

---
+++ ApplicationTerminal/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ApplicationTerminal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            Console.WriteLine(command);
+            Console.WriteLine("Finalizado com sucesso!");
+        }
+    }
+}
